4039b339b5a35e2cd60d003bcbf890d8f5231b58
Peter McDaniel <kg.mcd@hotmail.com>
2017-07-27 12:39:59 +0200

    last flask try

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, jsonify
+from flask import Request
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -8,7 +9,14 @@ def hello():
     print("hello")
     return render_template("./home.html")
 
+@app.route("/interactive/")
+def background_page():
+    return render_template("interactive.html")
 
-
+@app.route("/background_process")
+def background_process():
+    first = Request.args.get("first_name")
+    last = Request.args.get("last_name")
+    return jsonify(first_name=first, last_name=last)
 if __name__ == "__main__":
     app.run()
--- FlaskApp/FlaskApp/static/js/main.js
+++ FlaskApp/FlaskApp/static/js/main.js
@@ -1,9 +1,17 @@
-$(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-
+$(document).ready(function () {
     $('#calendar').fullCalendar({
         // put your options and callbacks here
     })
 
+});
+$(function () {
+    $('a#done').bind('click', function () {
+        $.getJSON('/background_process', {
+            first_name: $('input[name="first_name"]').val(),
+            last_name: $('input[name="last_name"]').val()
+        }, function (data) {
+            $("#result").text(data.first_name);
+        });
+        return false;
+    });
 });
